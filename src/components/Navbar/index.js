0b8e10d9dd586a34898a2c433cc145e84201f0f4
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-06 00:21:56 -0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,16 +11,16 @@ class Navbar extends Component {
         <Col md={1}>
           <Logo></Logo>
         </Col>
-        <Col md={{ span : 1, offset : 7}}>
+        <Col md={{ span : 1, offset : 5}}>
           ABOUT
         </Col>
-        <Col md={1}>
+        <Col md={{span : 1, offset : 0.5}}>
           MEMBERS
         </Col>
-        <Col md={1}>
+        <Col md={{span : 1, offset : 0.5}}>
           CONTACT
         </Col>
-        <Col md={1}>
+        <Col md={{span : 1, offset : 0.5}}>
           GALLERY
         </Col>
       </Row>
