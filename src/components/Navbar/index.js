1bb70dd8429733dc161211770258b6e111b535c8
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-06 00:25:58 -0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {withRouter, Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../aabT.svg';
+import {ReactComponent as Logo} from '../../aab_logo.svg';
 import {Row, Col} from 'react-bootstrap';
 import './styles.scss';
 
@@ -14,13 +14,13 @@ class Navbar extends Component {
         <Col md={{ span : 1, offset : 5}}>
           ABOUT
         </Col>
-        <Col md={{span : 1, offset : 0.5}}>
+        <Col md={{span : 1, offset : 0.66}}>
           MEMBERS
         </Col>
-        <Col md={{span : 1, offset : 0.5}}>
+        <Col md={{span : 1, offset : 0.66}}>
           CONTACT
         </Col>
-        <Col md={{span : 1, offset : 0.5}}>
+        <Col md={{span : 1, offset : 0.66}}>
           GALLERY
         </Col>
       </Row>
