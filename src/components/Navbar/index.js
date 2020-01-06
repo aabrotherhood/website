1df194a068709fb1b158b753012e313745ac9a5a
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-05 23:28:37 -0500

    Create index.js
    created navbar index file

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {withRouter, Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../aabT.svg';
+import {Row, Col} from 'react-bootstrap';
+
+class Navbar extends Compenent {
+  render () {
+    return (
+      <Row>
+        <Col>
+
+        <\Col>
+      </Row>
+}
+}
+
+export default withRouter(Navbar);
