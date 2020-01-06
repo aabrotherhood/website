ca9ca8d2316795a2a488ab1d42f787c2c4191753
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-06 00:09:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route} from 'react-router-dom';
 import './index.css';
 import App from './components/App/index';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}>
+    <App>
+      <Route exact path="/" render={Home}/>
+    </App>
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
