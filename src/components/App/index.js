9cd7530a2214363b27c72d8efd950dc93288bbb0
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-05 23:22:06 -0500

    Update index.js
    I redid everything I deleted because I realized I was editing the wrong file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,35 +1,26 @@
-import React, {Component} from 'react';
-import {withRouter, Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../aabT.svg';
-import {Row, Col} from 'react-bootstrap';
+import React from 'react';
+import logo from '../../logo.svg';
 import './styles.scss';
 
-class NavBar extends Compenent {
-  render () {
-    return (
-      <Row>
-        <Col>
-          <Link className="link" to="/"><Logo></Logo></Link>
-        </Col>
-      </Row>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
