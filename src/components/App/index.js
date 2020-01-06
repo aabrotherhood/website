d3959714c5826df73e7feb2d0450a4daea50f6e2
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-05 23:57:33 -0500

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import logo from '../../logo.svg';
+import {withRouter} from 'react-router-dom';
 import Navbar from '../Navbar/index';
 import './styles.scss';
 
