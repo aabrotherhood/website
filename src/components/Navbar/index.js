c89f79102604f7fce86d66dab56781d2e56ad52a
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-05 23:34:39 -0500

    Update index.js
    tentative start to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,16 +2,30 @@ import React, {Component} from 'react';
 import {withRouter, Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../aabT.svg';
 import {Row, Col} from 'react-bootstrap';
+import './styles.scss';
 
 class Navbar extends Compenent {
   render () {
     return (
       <Row>
+        <Col md={1}>
+          <Link className="link" to="/"><Logo></Logo></Link>
+        </Col>
+        <Col md={{ span = 1, offset = 3}}>
+          ABOUT
         <Col>
-
-        <\Col>
+        <Col md={1}>
+          MEMBERS
+        <Col>
+        <Col md={1}>
+          CONTACT
+        <Col>
+        <Col md={1}>
+          GALLERY
+        <Col>
       </Row>
+     );
 }
-}
+};
 
 export default withRouter(Navbar);
