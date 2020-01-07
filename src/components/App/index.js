1353363410b301f78b6df1edc257a73d1a8d83ee
Abraham Lee <leeaj@college.harvard.edu>
2020-01-06 23:10:16 -0500

    Abe (#5)
    * updated file structure and added scss
    
    * HomeHeroPic with mediaqueries and variables
    
    * Changed app back
    
    * changed font
    
    * finished Mission component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import logo from '../../logo.svg';
 import Navbar from '../Navbar/index';
 import {Container} from 'react-bootstrap';
 import HomeHero from '../HomeHero/index';
+import Mission from'../Mission/index';
 import '../../styles/global.scss';
 import './styles.scss';
 
@@ -12,6 +13,7 @@ class App extends Component {
     <Container className="App">
       <Navbar/>
      <HomeHero/>
+     <Mission/>
     </Container>
   );
 }
---
+++ src/components/Mission/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Col} from 'react-bootstrap';
+import './styles.scss';
+
+class Mission extends Component {
+  render() {
+    return (
+      <Col className="mission">
+        <p className="missionTitle">Our Mission</p>
+        <p className="missionDetails">AAB is built on three pillars: service, brotherhood, and activism</p>
+      </Col>
+    );
+  }
+}
+
+export default Mission;
