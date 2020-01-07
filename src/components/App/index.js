8914ff6f02fba9787ccceb86dfae9512aa975e4f
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-06 20:46:57 -0500

    Amia (#4)
    * hello
    
    * Add files via upload
    
    added logo
    
    * Update index.js
    
    started on menu bar but went to sleep lol
    
    * Update index.js
    
    * Update index.js
    
    I redid everything I deleted because I realized I was editing the wrong file
    
    * added navbar folder and logo
    
    * Create index.js
    
    created navbar index file
    
    * Update index.js
    
    tentative start to navbar
    
    * Create styles.scss
    
    * Update index.js
    
    testing navbar insertion??
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Create index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Delete aabT.png
    
    * Add files via upload
    
    * Delete aab_logo.svg
    
    * Add files via upload
    
    * Delete aab_logo.svg
    
    * Add files via upload
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.scss
    
    * Update index.js
    
    * commiting package.json changes
    
    * just making sure?

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import logo from '../../logo.svg';
+import Navbar from '../Navbar/index';
 import {Container} from 'react-bootstrap';
 import HomeHero from '../HomeHero/index';
 import '../../styles/global.scss';
 import './styles.scss';
 
-function App() {
+class App extends Component {
+  render () {
   return (
     <Container className="App">
+      <Navbar/>
      <HomeHero/>
     </Container>
   );
+}
 }
 
 export default App;
