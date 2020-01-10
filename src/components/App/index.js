90f52f6971fb7e5a82a2c5a2d5c701f137221a34
akr4500 <39604072+akr4500@users.noreply.github.com>
2020-01-10 00:18:57 -0500

    Amia (#8)
    * updated file structure and added scss
    
    * hello
    
    * HomeHeroPic with mediaqueries and variables
    
    * Changed app back
    
    * Add files via upload
    
    added logo
    
    * Update index.js
    
    started on menu bar but went to sleep lol
    
    * Update index.js
    
    * Update index.js
    
    I redid everything I deleted because I realized I was editing the wrong file
    
    * added navbar folder and logo
    
    * Create index.js
    
    created navbar index file
    
    * Update index.js
    
    tentative start to navbar
    
    * Create styles.scss
    
    * Update index.js
    
    testing navbar insertion??
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Create index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Delete aabT.png
    
    * Add files via upload
    
    * Delete aab_logo.svg
    
    * Add files via upload
    
    * Delete aab_logo.svg
    
    * Add files via upload
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * changed font
    
    * Update styles.scss
    
    * Update index.js
    
    * commiting package.json changes
    
    * just making sure?
    
    * finished Mission component
    
    * fixed the navbar finally
    
    * fixed navbar and homescreen a little
    
    * started brotherhood
    
    * finished brotherhood
    
    * added Brotherhood
    
    * started activism
    
    * finished activism
    
    * added activism
    
    * added the assets
    
    * fixed positioning of brotherhood and activism
    
    * finished service
    
    * Updated mediaqueries and components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import logo from '../../logo.svg';
 import Navbar from '../Navbar/index';
 import {Container} from 'react-bootstrap';
 import HomeHero from '../HomeHero/index';
 import Mission from'../Mission/index';
 import Brotherhood from '../Brotherhood/index';
 import Activism from '../Activism/index';
+import Service from '../Service/index';
 import '../../styles/global.scss';
 import './styles.scss';
 
@@ -13,11 +13,13 @@ class App extends Component {
   render () {
   return (
     <Container className="App">
-      <Navbar/>
-     <HomeHero/>
-     <Mission/>
-     <Brotherhood/>
-     <Activism/>
+      <Navbar/> 
+      <HomeHero/>
+      <Mission/>
+      <Brotherhood/>
+      <Activism/>
+      <div className="spacer"></div>
+      <Service/>
     </Container>
   );
 }
--- src/components/Brotherhood/index.js
+++ src/components/Brotherhood/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Img from 'react-image';
-import {Col, Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import Preloader from '../Preloader/index';
 import Bro1 from '../../assets/bro1.jpg';
 import Bro2 from '../../assets/bro2.jpg';
@@ -11,23 +11,15 @@ class Brotherhood extends Component {
 	render () {
 		return (
 			<Col className="broCol">
-				<Row>
-					<Img className='bro1' src={Bro1} loader={<Preloader/>} alt='2018 Name Day'/>
-				</Row>
-				<Row className='justify-content-end'>
-					<Col>
-						<Img className='stripes' src={Stripes} loader={<Preloader/>} />
-					</Col>
-					<Col>
-						<Img className='bro2' src={Bro2} loader={<Preloader/>} alt='2018 Senior Retreat'/>
-					</Col>
+				<Img className='bro1' src={Bro1} loader={<Preloader/>} alt='2018 Name Day'/>
+				<Img className='stripes' src={Stripes} loader={<Preloader/>} />
+				<Img className='bro2' src={Bro2} loader={<Preloader/>} alt='2018 Senior Retreat'/>
 					<Col className='brotherhoodText'>
-						<p className='brotherhoodTitle'>BROTHERHOOD</p>
+						<p className='brotherhoodTitle'>Brotherhood</p>
 						<p className='brotherhoodDetails'>
 							Our close-knit membership provides every member the opportunity to be an active and contributing force within the organization.
 						</p>
 					</Col>
-				</Row>
 			</Col>
 		);
 	}
