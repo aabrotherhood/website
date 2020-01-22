52f63c61c88b0110d01cc96d57e82e16553c7555
Amia <amia@MacBook-9.lan>
2020-01-22 00:17:34 -0500

    members page

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import {withAuthorization} from '../Sessions';
 import {Container} from 'react-bootstrap';
 import Brothers from '../Brothers';
+import Navbar from '../Navbar/index';
 import './styles.scss';
 
 class Members extends Component {
   render() {
     return( 
-      <div className="members">
-        <Brothers/>
-      </div>
+    <Container>
+    	<Navbar/>
+    	<div className="members">
+    		<Brothers/>
+    	</div>
+    </Container>
     )
   }
 }
