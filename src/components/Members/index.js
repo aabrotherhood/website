70ccd3e0e68a3a86f2f9fe5fc27cb2f0b3cd96b6
Amia <amia@MacBook-9.lan>
2020-01-21 20:05:15 -0500

    oops i tried

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import {withAuthorization} from '../Sessions';
+import {Container} from 'react-bootstrap';
 import Brothers from '../Brothers';
+import './styles.scss';
 
-class Home extends Component {
+class Members extends Component {
   render() {
     return( 
-      <div>
-        <Brothers/>
-      </div>
+    	<Container>
+    		<div>
+        		<Brothers className='members'/>
+    		</div>
+    	</Container>
     )
   }
 }
 
-const condition = authUser => !!authUser;
-
-export default withAuthorization(condition)(Home);
+export default (Members);
