5439a66bcdd23aa2d95043517e36a8572d6fcf69
Abraham Lee <leeaj@college.harvard.edu>
2020-02-01 17:15:57 -0500

    Abe firebase (#13)
    * updated file structure and added scss
    
    * HomeHeroPic with mediaqueries and variables
    
    * Changed app back
    
    * changed font
    
    * finished Mission component
    
    * Started Contact
    
    * setup scroll to different components from navbar + routes
    
    * more Routes!
    
    * finished routes
    
    * created separate routes.js file
    
    * added basic session and firebase signup login
    
    * implemented higher order authentication and authorization component
    
    * finished authorization routes
    
    * finished edit logic
    
    * changed imageURL
    
    * done with BrotherList
    
    * added in Members component
    
    * added in animations
    
    * added in contact
    
    * added in style sheets to members
    
    * oops i tried
    
    *  did some things
    
    * updated things
    
    * huge update with recruits sign up
    
    * another huge one with finished comments
    
    * finished navbar
    
    * updated firebase config
    
    * added in redirects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router-dom';
+import {Router, Route } from 'react-router-dom';
 import history from './history.js';
 import './index.scss';
 import App from './components/App/index';
