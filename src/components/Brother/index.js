2ed30bb14dda048de3ca31d8bd02285f3c574779
Abraham Lee <abrahamlee@dhcp-10-250-58-205.harvard.edu>
2020-02-03 19:49:42 -0500

    added loading gif

--- src/components/Brother/index.js
+++ src/components/Brother/index.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
 import {Col} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom';
+import Loading from '../../assets/loading.gif';
 
 
 class Brother extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {loaded: false};
+    this.handleImageLoaded = this.handleImageLoaded.bind(this);
+  }
+
+  handleImageLoaded() {
+    this.setState({loaded: true})
+  }
   render() {
     const brotherImage = {
       backgroundImage: 'url(' + this.props.info.imageURL + ')',
@@ -11,9 +22,18 @@ class Brother extends Component {
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
     }
+    const imageLoading = {
+      display: 'none',
+    }
+    const loadingGif = {
+      width: '75px',
+      height: '75px'
+    }
     return (
       <Col onClick={() => this.props.history.push(this.props.nextURL, {data:JSON.stringify(this.props.info)})} xs={3} className="brother">
-        <div className="brotherImage" style={brotherImage}></div>
+        <img src={this.props.info.imageURL} onLoad={this.handleImageLoaded} alt="bro" style={imageLoading}/>
+        {this.state.loaded ?<div className="brotherImage" style={brotherImage}></div> :
+          <div className="brotherImage" style={loadingGif}><img src={Loading} alt="loading"/></div> }
         <div className="brotherInfo">
           <h2 className="realName">{this.props.info.first + ' ' + this.props.info.last}</h2>
           <p className="brotherName">{this.props.info.brotherName}</p>
