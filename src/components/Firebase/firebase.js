b33b5c4ce50cd9997962f68ca796914da2824f82
Abraham Lee <abrahamlee@Abrahams-MBP-2.T-mobile.com>
2020-06-08 20:25:48 -0400

    updated to use dev config when in dev mode

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -24,7 +24,7 @@ const firebaseConfigDev = {
   measurementId: "G-1K0MZFX1CJ"
 };
 
-const config =  firebaseConfig;
+const config =  process.env.NODE_ENV === 'production' ? firebaseConfig : firebaseConfigDev;
 
 
 class Firebase {
