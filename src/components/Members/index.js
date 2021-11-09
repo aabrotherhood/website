2f2305755ea03f726ebfb3ef7577d73af4911d02
akr4500 <akr4500@gmail.com>
2021-11-09 00:31:24 -0500

    changed members picture

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Brothers from '../Brothers';
 import {Col} from 'react-bootstrap';
 import Bro2 from '../../assets/activism.jpeg';
+import Bro3 from '../../assets/AAB.jpg';
 import {Parallax} from 'react-parallax';
 import {withFirebase} from '../Firebase/context';
 import {AuthUserContext} from '../Sessions'
@@ -15,8 +16,8 @@ class Members extends Component {
         <Parallax
           className="homeHeroPic"
             blur={{ min: -3, max: 7 }}
-            bgImage={Bro2} 
-            bgImageAlt="2019 group"
+            bgImage={Bro3} 
+            bgImageAlt="2020 group"
             strength={-100}>
               <p className="homeLogo">Meet our Brothers</p>
             </Parallax>
