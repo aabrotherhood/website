26b03d73da5c64ac164dfb5be582d79496715a46
akr4500 <akr4500@gmail.com>
2022-01-29 02:37:33 -0500

    full restore

--- src/components/RecruitmentSignUp/index.js
+++ src/components/RecruitmentSignUp/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Col, Form, InputGroup, Button, Row} from 'react-bootstrap';
 import Logo from '../../assets/aab_logo.svg';
 import Img from 'react-image';
-// import imageCompression from 'browser-image-compression';
 import Preloader from '../Preloader/index';
 import {withFirebase} from '../Firebase';
 
@@ -18,10 +17,11 @@ class RecruitmentSignUp extends Component {
       phone: '',
       personalEmail: '',
       schoolEmail: '',
-      // image: null
+      imageURL: 'https://firebasestorage.googleapis.com/v0/b/aab-website-754b0.appspot.com/o/brothers%2Faab.png?alt=media&token=459771fb-0788-4f19-912a-ad3cfbc6de3f',
     }
 
     this.initialState = this.state
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.resetForm = this.resetForm.bind(this);
@@ -32,6 +32,7 @@ class RecruitmentSignUp extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+
     const recruitUID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     const currentRecruit = this.props.firebase.recruit(recruitUID);
     const {first, last, house, year, phone, personalEmail, schoolEmail, imageURL} = this.state
@@ -45,79 +46,11 @@ class RecruitmentSignUp extends Component {
       }
     })
     this.resetForm();
-
-  }
-    // this.handleImageUpload = this.handleImageUpload.bind(this);
-//   handleSubmit(event) {
-//     event.preventDefault();
- 
-//     const recruitUID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-//     const currentRecruit = this.props.firebase.recruit(recruitUID);
-//     const {first, last, house, year, phone, personalEmail, schoolEmail} = this.state
-//     if (this.state.image !== null) {
-//       currentRecruit.put(this.state.image).then(() => {
-//         console.log('Successfully loaded image');
-//         currentRecruit.getDownloadURL().then(imageURL=> {
-//           currentRecruit.set({
-//             first,last, house, year,
-//             phone, personalEmail, schoolEmail, imageURL}
-//             ,function(error) {
-//               if (error) {
-//                 // The write failed...
-//                 console.log('Failed updated recruit info');
-//               } else {
-//                 // Data saved successfully!
-//                 console.log('Succesfully updated information');
-//               }
-//             })
-//             this.resetForm();
-//         })
-//       }).catch(err => {
-//         console.log('ERROR', err);
-//       })
-//     } else {
-//       const imageURL = "https://firebasestorage.googleapis.com/v0/b/aab-website-754b0.appspot.com/o/brothers%2Faab.png?alt=media&token=459771fb-0788-4f19-912a-ad3cfbc6de3f";
-//       currentRecruit.set({
-//         first, last, house, year, phone,personalEmail, schoolEmail, imageURL
-//       }, function(error) {
-//         if (error) {
-//           console.log('something went wrong',error);
-//         } else {
-//           console.log('registered new recruit');
-//         }
-//       })
-//     this.resetForm();
-//   }
-// }
-
-  // async handleImageUpload(event) {
- 
-  //   const imageFile = event.target.files[0];
-  //   console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
-  //   console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
    
-  //   var options = {
-  //     maxSizeMB: 0.1,
-  //     maxWidthOrHeight: 1024,
-  //     useWebWorker: true
-  //   }
-  //   try {
-  //     const compressedFile = await imageCompression(imageFile, options);
-  //     console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
-  //     console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
-  //     this.setState({image: compressedFile}, () => console.log('set image'));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
+  }
   handleChange(event) {
-  //   if (event.target.files) {
-  //     this.handleImageUpload(event);
-  //   } else {
-       this.setState({[event.target.name]: event.target.value});
-  //   }
-   }
+      this.setState({[event.target.name]: event.target.value});
+  }
 
   render() {
     return( 
@@ -126,23 +59,6 @@ class RecruitmentSignUp extends Component {
           <Img className='navbarLogo' src={Logo} loader={<Preloader/>} alt='AAB logo'/>
         </Row>
         <Form noValidate onSubmit={this.handleSubmit} className="customForm">
-        {/* <Form.Row>
-            <Form.Group as={Col} md="12">
-            <Form.Label>Profile Picture</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  type="file"
-                  name="image"
-                  onChange={this.handleChange}
-                  className="formImage"
-                  required
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please submit a picture.
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Form.Row> */}
           <Form.Row>
             <Form.Group as={Col} md="6">
               <Form.Label>First</Form.Label>
@@ -155,7 +71,7 @@ class RecruitmentSignUp extends Component {
                   required
                 />
                 <Form.Control.Feedback type="invalid">
-                  Please enter your first name.
+                  Please enter your brother name.
                 </Form.Control.Feedback>
               </InputGroup>
             </Form.Group>
