fea7cc1f40457ee2327cb631b3d6945c419ffbbe
Brandon Kim <brandon@dhcp-10-250-101-7.harvard.edu>
2023-01-31 14:08:28 -0500

    activating recruitment

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import Recruits from './components/Recruits';
 import RecruitmentComments from './components/RecruitmentComments';
 import Emails from './components/Emails';
 import RecruitmentImages from './components/RecruimentImages';
-import Meals from './components/Meals';
+// import Meals from './components/Meals';
 // End recruitment code
 import * as serviceWorker from './serviceWorker';
 import * as ROUTES from './constants/routes';
@@ -40,12 +40,12 @@ ReactDOM.render(
         <Route path={ROUTES.HOME} component={Landing}></Route>
         <Route path={ROUTES.EDIT} component={Edit}></Route>
         <Route path={ROUTES.INDBROTHER} component={IndBrother}></Route>
-        {/* <Route exact path={ROUTES.RECRUITMENTSIGNUP} component={RSignUp}></Route>
+        <Route exact path={ROUTES.RECRUITMENTSIGNUP} component={RSignUp}></Route>
         <Route path={ROUTES.RECRUITMENT} component={Recruits}></Route>
         <Route path={ROUTES.INDRECRUIT} component={IndRecruit}></Route>
         <Route path={ROUTES.RECRUITMENTCOMMENTS} component={RecruitmentComments}></Route>
         <Route path={ROUTES.RECRUITMENTEMAILS} component={Emails}/>
-        <Route path={ROUTES.RECURITMENTIMAGES} component={RecruitmentImages}/> */}
+        <Route path={ROUTES.RECURITMENTIMAGES} component={RecruitmentImages}/> 
         {/* <Route path={ROUTES.MEALS} component={Meals}/> */}
         {/* <Route path={ROUTES.FINAL} component={Final}/> */}
         {/* <Route path={ROUTES.DELIBS} component={Delibs}/> */}
