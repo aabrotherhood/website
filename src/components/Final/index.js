7d6f001b6d9c0ff6905465a61ce7e26da488a119
Brandon Kim <brandon@dhcp-10-250-101-7.harvard.edu>
2023-03-02 13:25:21 -0500

    fixing voting results page

--- src/components/Final/index.js
+++ src/components/Final/index.js
@@ -15,10 +15,6 @@ class Final extends Component {
         '5sh19dgh3dnnk8c7fstp': "Henry Xuan",
         '5u7vjkmkdhs8tex3f8xd9a': "Justin Liu",
         '7tp8c874unoklhbk13vdd': "Adam Mohamed",
-<<<<<<< HEAD
-        '7voa5ovztsjqrzr3ususq': "Cyril Leahy",
-=======
->>>>>>> a4842ddca0bc443bf2d6c564a71a98533b650a60
         '88shi35m5ji82hemr7r8el': "Sean Meng",
         '8fhm29j59rhb2o1v6m84k': "Albert Shin",
         '8hzdceh877jyt8lq54417': "Dylan Rhee", 
@@ -33,10 +29,6 @@ class Final extends Component {
         'i6zc72qj8fjviw1d8pvim9': "Lucie Bai", 
         'i8405c2mg58wluy19tei9': "Connor Lee", 
         'irl9p0suuxjsouk4ujwwe': "Beier Nelson", 
-<<<<<<< HEAD
-        'j74hhwkua8p69dd2ceel': "Jang Choe", 
-=======
->>>>>>> a4842ddca0bc443bf2d6c564a71a98533b650a60
         'joxmf1kaysb2lsiov4xfjb': "Victor Ghosh", 
         'khtjyhi15pgrvz5esfl8j': "Minkai Li", 
         'kwozd2fs34amzjunqu28f': "Nick Gu", 
@@ -50,300 +42,187 @@ class Final extends Component {
         'qds5qs4m46jmhlj84tt6al': "Derek Zheng",
         'rvmvpx4iq2nj5ndkhi7hu': "Dylan Jin-Ngo",
         'rwwqzx9aopcsw0iifqocp': "Darren Tran",
-<<<<<<< HEAD
-        'ss8cgltmq5xn5a9wlpios': "Daniel Wang",
-        'uh37dt05t176mkxzck765t': "Ali Khan",
-        'w5g1doaefnon6356r49r8': "Atticus Lee",
-        'ys8siehhxshfiyq6zztnfl': "Aidan Zhou", 
-        'yua2bcfchhdrfnqiogunz': "Lauryn Wilson",      },
-=======
         'w5g1doaefnon6356r49r8': "Atticus Lee",
         'ys8siehhxshfiyq6zztnfl': "Aidan Zhou", 
         'yua2bcfchhdrfnqiogunz': "Lauryn Wilson",  },
->>>>>>> a4842ddca0bc443bf2d6c564a71a98533b650a60
         loading: false,
         'recruitVotes': {
-          '4z34771kuwc6d8i95ewcj6': {
+          '2zg2ucutigk9rgcwj0joxr': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'zzikerjbn1mmbfvf5w0t9b': {
+          '33lsglc0b621ux54n4jgb7': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'yxddr58djj1934akgwt09': {
+          '512fa2i0cv84a1o37gbdan': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'yusrxvveqc9zrq6pbxh4u': {
+          '5sh19dgh3dnnk8c7fstp': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ynftqgjg50bsm7xko0y5ph': {
+          '5u7vjkmkdhs8tex3f8xd9a': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ym3uapfd8cda0aixzj18': {
+          '7tp8c874unoklhbk13vdd': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'xklj6ch21uiagtoa1z9v': {
+          '88shi35m5ji82hemr7r8el': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'xcad8d8v4pttbwmvokwb': {
+          '8fhm29j59rhb2o1v6m84k': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'wm9ofgs8u1e43uo24akdf': {
+          '8hzdceh877jyt8lq54417': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'w05gtcxo0q8k4iq31rcw1': {
+          '9m7ve4wbp6g8f68iutz42b': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'uy3zdxtpdl0648po92o47': {
+          'be1ts50h4b5ctjp5h0y96c': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ult0gs9tr2l4insekb1kfr': {
+          'eeomh5olx9etzkh1qk1eec': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'p05dldd8226tzcz8iplv6': {
+          'f53rp5ytvp5zq4z0igcqc8': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ocktjb9dezit7d1hgvaxt9': {
+          'fsd3mu2axurk0deynjs8uk': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'nudg9r0cizeb8y1mydhd2w': {
+          'gf32p5tsvwmftqd54742n': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'nm687p60epxn3c0tzepwh': {
+          'gujxqelc7hjyb1z84dahp': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'mvwa46yg7qpsbrgdky4wj': {
+          'h0t9714q3zsy8fnawz4td': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'lik034xotmn4udg8178': {
+          'i6zc72qj8fjviw1d8pvim9': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'l71qifa26gseyupgjr0uh': {
+          'i8405c2mg58wluy19tei9': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'l5jhd7r7i0qxvcc9v58i7o': {
+          'irl9p0suuxjsouk4ujwwe': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'kteaw0ckmdxj2cjbs764h': {
+          'joxmf1kaysb2lsiov4xfjb': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'jvdo2qmcqfih2nk6h65yk': {
+          'khtjyhi15pgrvz5esfl8j': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'jelhixvz7petwrpuk5buzg': {
+          'kwozd2fs34amzjunqu28f': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ja7n8l9nottlmy7dtx7qr': {
+          'mha40ib7cmj6km15409509': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'ihyjxonrfzq3sm4xit3n2i': {
+          'mnlwyf46ody0kxgrc91i': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'glw2gtq49zbhhh9yolp9f': {
+          'mwalfhyjo357ot3visndc': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'f7h4127i9af98jgdo91nq': {
+          'notn3kh17obrptqqp3ltn': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'dq1nepgirow935bpix741p': {
+          'o3vcfpnqd99s9dsjtyjsl': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'dmp5cbagdbxtdq1szrgz': {
+          'q4pdzs9av1dtlihcqoqy3k': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'daowiae9cmd7vz5aq0ov13': {
+          'q8a1a9qk6sqtquuizop6r': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'cstftpj2zj6shbcnuxwh4k': {
+          'qds5qs4m46jmhlj84tt6al': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'cmyv6rrx9s4tm3t4skgwy': {
+          'rvmvpx4iq2nj5ndkhi7hu': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'cihl5fzlc7ln1w3t4s9et7': {
+          'rwwqzx9aopcsw0iifqocp': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'c9914cyoxupb8e3yegxgc': {
+          'w5g1doaefnon6356r49r8': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'c71yc8tpxoou794clvcfwf': {
+          'ys8siehhxshfiyq6zztnfl': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
           },
-          'by4926397stzi19higab': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          'aetbs9fw2mdmy4sqifeg1n': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          'abjzgkws9qfktbandic54j': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          'a6wg4iwxwl2zmf6f3lw3d': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          'a5c37qstumd8zup8prwr': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '9cy1svpf0f9r6a85tarxri': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '97jm0zsc4en59rl17fdoi': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '8qem8sszp47bwsweuuzgva': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '8ejrwsgbiib4d0e1sz3n': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '89jxige59jwhzhkcmptvyn': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '7on0klgvusrhm3l1aznsbc': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '6snsz913q5q3g7xpzwhoyy': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '6oihxhrsqe8g32g21xg58f': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '5gkvrlmzi7wgru4kx6lxqr': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '4x339jcg2ksqbc8dpg45sg': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '02r1a6ac67tp89zk5hivg': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '1zzemht7in7yu6zb1gleko': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '2xumgeav1iur30stz5128o': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '3ycu9umcc2n7aiufr5z4ok': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '43b86qc0nxpjuxvn1falvc': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '4fcme6a940aenxx735fc46': {
-            'yes': 0,
-            'no': 0,
-            'maybe': 0,
-          },
-          '4k35ivi0bki9m2wrt18zs': {
+          'yua2bcfchhdrfnqiogunz': {
             'yes': 0,
             'no': 0,
             'maybe': 0,
