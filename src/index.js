a63744ba448f78597eda1314199299efebc09b00
b0kim <71298559+b0kim@users.noreply.github.com>
2025-01-29 21:01:22 -0500

    reset webwite for recruitment 2025

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import CustomForm from './components/CustomFormWrapper/index';
 import Edit from './components/Edit/index';
 import IndBrother from './components/IndBrother';
 // This is recruitment code
-// import IndRecruit from './components/IndRecruit';
-// import RSignUp from './components/RecruitmentSignUp';
-// import Recruits from './components/Recruits';
-// import RecruitmentComments from './components/RecruitmentComments';
-// import Emails from './components/Emails';
-// import RecruitmentImages from './components/RecruimentImages';
-// import Meals from './components/Meals';
+import IndRecruit from './components/IndRecruit';
+import RSignUp from './components/RecruitmentSignUp';
+import Recruits from './components/Recruits';
+import RecruitmentComments from './components/RecruitmentComments';
+import Emails from './components/Emails';
+import RecruitmentImages from './components/RecruimentImages';
+import Meals from './components/Meals';
 // End recruitment code
 import * as serviceWorker from './serviceWorker';
 import * as ROUTES from './constants/routes';
@@ -40,17 +40,17 @@ ReactDOM.render(
         <Route path={ROUTES.HOME} component={Landing}></Route>
         <Route path={ROUTES.EDIT} component={Edit}></Route>
         <Route path={ROUTES.INDBROTHER} component={IndBrother}></Route>
-        {/* <Route exact path={ROUTES.RECRUITMENTSIGNUP} component={RSignUp}></Route>
+        <Route exact path={ROUTES.RECRUITMENTSIGNUP} component={RSignUp}></Route>
         <Route path={ROUTES.RECRUITMENT} component={Recruits}></Route>
         <Route path={ROUTES.INDRECRUIT} component={IndRecruit}></Route>
         <Route path={ROUTES.RECRUITMENTCOMMENTS} component={RecruitmentComments}></Route>
         <Route path={ROUTES.RECRUITMENTEMAILS} component={Emails}/>
         <Route path={ROUTES.RECURITMENTIMAGES} component={RecruitmentImages}/> 
         <Route path={ROUTES.MEALS} component={Meals}/> 
-        <Route path={ROUTES.FINAL} component={Final}/> 
+        {/* <Route path={ROUTES.FINAL} component={Final}/> 
         <Route path={ROUTES.DELIBS} component={Delibs}/>
         <Route exact path={ROUTES.DELIBSORDER} component={DelibsOrder}/>
-        <Route exact path={ROUTES.DELIBSRECRUIT} component={DelibsRecruit}/>  */}
+        <Route exact path={ROUTES.DELIBSRECRUIT} component={DelibsRecruit}/>   */}
       </App>
     </Router>
   </FirebaseContext.Provider>,
